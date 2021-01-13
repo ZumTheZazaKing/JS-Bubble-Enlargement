0f4bd58f53a673d22f83963d471bda317fd47c20
Purogurama Z <73376015+ZumTheZazaKing@users.noreply.github.com>
2021-01-13 17:58:32 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function enlargen(circle){
+
+    let counter = 20;
+
+    circle.style.transform = 'scale(' + counter + ')';
+
+
+}
+
+function stopEnlargement(circle){
+
+    counter = 1;
+
+    circle.style.transform = 'scale(' + counter + ')';
+
+
+}
